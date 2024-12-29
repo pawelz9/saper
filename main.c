5a956987cbb74e2a29bf4c02d99c89ad075b3b63
pawelz9 <pawelzawisl@gmail.com>
2024-12-29 23:15:46 +0100

    change branch+small changes

--- main.c
+++ main.c
@@ -1,70 +1,111 @@
 #include<stdio.h>
 #include <stdlib.h>
-
+#include<time.h>
 
 typedef struct saper
 {
-	int r;	
+	int r;
 	int c;
 	int mines;
 	int **tab;
-}saper ;	//zmienilem nazwe na saper bo latwiej sie czyta
+}saper;
 
 saper *init(int r, int c,int mines ){
-	saper *A=(saper*)malloc(sizeof(saper));
+	saper* A=(saper*)malloc(sizeof(saper));
 	A->r=r;
 	A->c=c;
 	A->mines=mines;
-	A->tab=(int**)calloc(r,sizeof(int*)); 
+	A->tab=(int**)calloc(r,sizeof(int*));
 	for(int i=0;i<r;i++)
 		A->tab[i]=(int *)calloc(c,sizeof(int));
 	return A;
-} 
-void free_sap(saper *A) {
+}
+ 
+void free_saper(saper* A) {
     for (int i = 0; i < A->r; i++) {
         free(A->tab[i]);
     }
     free(A->tab);
     free(A);
 }
-	
-int main()
-{
-	char x;
-	int k = 1; //musi byc jakas wartosc k bo inaczej to nie wejdzie do while
-	int r,c,mines;
-	saper *plansza;
 
-	printf ("GRA SAPER\n");
-	
-	do{
-		printf ("wybierz poziom trudnosci:\n");
-		printf ("l-latwy 9x9 10 min\n");
-		printf ("s-sredni 16x16 40 min\n");
-		printf ("t-trudny 16x30 99 min\n");
-		printf ("w-wlasna plansza\n");
-		scanf(" %c",&x); // spacja obok %c ignoruje biale znaki jak '\n' wiec trzeba na to uwazac
+//w losowa wspolrzedna przypisujemy mine
+saper *assign(saper *A)
+{
+	int row,col;
+	for(int i=0;i<A->mines;i++){
+		row=rand()%A->r;
+		col=rand()%A->c;
+		if(A->tab[row][col]==1){
+			i--;
+			continue;
+		}
+		A->tab[row][col]=1;
+
+	}
+
+	return A;
+}
+void sasiady(saper *A,int **tab2){
+	//tab2 liczy ile min w sasiedztwie
+
+
+}
+
+saper *start(saper *A){
+	char x;
+	int k=1;
+	int r,c,mines;
+	printf ("GRA saperER\n");
+	printf ("wybierz poziom trudnosci:\n");
+	printf ("l-latwy 9x9 10 min\n");
+	printf ("s-sredni 16x16 40 min\n");
+	printf ("t-trudny 16x30 99 min\n");
+	printf ("w-wlasna plansza\n");
+	while(k){
+		k=0;
+		scanf(" %c",&x);
 		if(x=='l')
-			plansza = init(9,9,10);
+			A=init(9,9,10);
 		else if(x=='s')
-			plansza = init(16,16,40);
+			A=init(16,16,40);
 		else if(x=='t')
-			plansza = init(16,30,99);
+			A=init(16,30,99);
 		else if (x=='w'){
 			printf("\npodaj dlugosc: ");
 			scanf("%i",&c);
 			printf("podaj szerokosc: ");
 			scanf("%i",&r);
-		//warunek by realistyczna liczba min
-			printf("podaj ilosc min: ");
-			scanf("%i",&mines);
-			init(r,c,mines);
-		}else{
-			printf("Nieprawidlowa wartosc, wpisz jeszcze raz: \n");
-			k += 1;
+			k=1;
+			while(k){
+				k=0;
+				printf("podaj ilosc min: ");
+				scanf("%i",&mines);
+				if(mines>r*c*0.7 || mines <1){
+					printf("nieprawidlowa ilosc min\n");
+					k=1;
+				}
+			}
+			A=init(r,c,mines);
 		}
-		k -= 1;
-	}while(k!= 0);
-	
+		else{
+			printf("nieprawidlowa wartosc wpisz jeszcze raz: ");
+			k=1;
+		}
+	}
+	return A;
+}
+int main()
+{
+	//ziarno
+	srand((unsigned int)time(NULL)^getpid());
+	//plansza <=> A
+	saper*A=NULL;
+	A=start(A);
+	A=assign(A);
+	int **tab2=(int**)calloc(A->r,sizeof(int*));
+	for(int i=0;i<A->r;i++)
+		tab2=(int *)calloc(A->c,sizeof(int));
+	free_saper(A);
 	return 0;
 }
