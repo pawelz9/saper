35b5605383c07fab81f363f21f33c29e7da97dfc
Delfinek777 <01197814@pw.edu.pl>
2024-12-29 15:18:27 +0100

    dodałem pare poprawek, wszystko jest w komentarzach (tez pisz komentarze plis)

--- main.c
+++ main.c
@@ -1,25 +1,26 @@
 #include<stdio.h>
+#include <stdlib.h>
 
 
-typedef struct sap
+typedef struct saper
 {
-	int r;
+	int r;	
 	int c;
 	int mines;
 	int **tab;
-}sap ;
+}saper ;	//zmienilem nazwe na saper bo latwiej sie czyta
 
-sap *init(int r, int c,int mines ){
-	sap* A=(sap*)malloc(sizeof(sap));
+saper *init(int r, int c,int mines ){
+	saper *A=(saper*)malloc(sizeof(saper));
 	A->r=r;
 	A->c=c;
 	A->mines=mines;
-	A->tab=(int**)calloc(r,sizeof(int*));
+	A->tab=(int**)calloc(r,sizeof(int*)); 
 	for(int i=0;i<r;i++)
 		A->tab[i]=(int *)calloc(c,sizeof(int));
 	return A;
 } 
-void free_sap(sap* A) {
+void free_sap(saper *A) {
     for (int i = 0; i < A->r; i++) {
         free(A->tab[i]);
     }
@@ -30,23 +31,25 @@ void free_sap(sap* A) {
 int main()
 {
 	char x;
-	int k;
+	int k = 1; //musi byc jakas wartosc k bo inaczej to nie wejdzie do while
 	int r,c,mines;
+	saper *plansza;
+
 	printf ("GRA SAPER\n");
-	printf ("wybierz poziom trudnosci:\n");
-	printf ("l-latwy 9x9 10 min\n");
-	printf ("s-sredni 16x16 40 min\n");
-	printf ("t-trudny 16x30 99 min\n");
-	printf ("w-wlasna plansza\n");
-	while(k!=0){
-		k=0;
-		scanf("%c",&x);
+	
+	do{
+		printf ("wybierz poziom trudnosci:\n");
+		printf ("l-latwy 9x9 10 min\n");
+		printf ("s-sredni 16x16 40 min\n");
+		printf ("t-trudny 16x30 99 min\n");
+		printf ("w-wlasna plansza\n");
+		scanf(" %c",&x); // spacja obok %c ignoruje biale znaki jak '\n' wiec trzeba na to uwazac
 		if(x=='l')
-			init(9,9,10);
+			plansza = init(9,9,10);
 		else if(x=='s')
-			init(16,16,40);
+			plansza = init(16,16,40);
 		else if(x=='t')
-			init(16,30,99);
+			plansza = init(16,30,99);
 		else if (x=='w'){
 			printf("\npodaj dlugosc: ");
 			scanf("%i",&c);
@@ -56,11 +59,12 @@ int main()
 			printf("podaj ilosc min: ");
 			scanf("%i",&mines);
 			init(r,c,mines);
+		}else{
+			printf("Nieprawidlowa wartosc, wpisz jeszcze raz: \n");
+			k += 1;
 		}
-		else{
-			printf("nieprawidlowa wartosc wpisz jeszcze raz: ");
-			k=1;
-		}
-	}
+		k -= 1;
+	}while(k!= 0);
+	
 	return 0;
 }
