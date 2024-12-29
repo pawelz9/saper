a68285667490e05eb3d7a0e861412b76315d8309
pawelz9 <pawelzawisl@gmail.com>
2024-12-29 19:28:30 +0100

    add assign +small changes

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-
+#include <stdlib.h>
+#include<time.h>
 
 typedef struct sap
 {
@@ -18,7 +19,8 @@ sap *init(int r, int c,int mines ){
 	for(int i=0;i<r;i++)
 		A->tab[i]=(int *)calloc(c,sizeof(int));
 	return A;
-} 
+}
+ 
 void free_sap(sap* A) {
     for (int i = 0; i < A->r; i++) {
         free(A->tab[i]);
@@ -26,11 +28,28 @@ void free_sap(sap* A) {
     free(A->tab);
     free(A);
 }
-	
-int main()
+
+//w losowa wspolrzedna przypisujemy mine
+sap *assign(sap *A)
 {
+	int row,col;
+	for(int i=0;i<A->mines;i++){
+		row=rand()%A->r;
+		col=rand()%A->c;
+		if(A->tab[row][col]==1){
+			i--;
+			continue;
+		}
+		A->tab[row][col]=1;
+
+	}
+
+	return A;
+}
+
+sap *start(sap *A){
 	char x;
-	int k;
+	int k=1;
 	int r,c,mines;
 	printf ("GRA SAPER\n");
 	printf ("wybierz poziom trudnosci:\n");
@@ -38,29 +57,48 @@ int main()
 	printf ("s-sredni 16x16 40 min\n");
 	printf ("t-trudny 16x30 99 min\n");
 	printf ("w-wlasna plansza\n");
-	while(k!=0){
+	while(k){
 		k=0;
-		scanf("%c",&x);
+		scanf(" %c",&x);
 		if(x=='l')
-			init(9,9,10);
+			A=init(9,9,10);
 		else if(x=='s')
-			init(16,16,40);
+			A=init(16,16,40);
 		else if(x=='t')
-			init(16,30,99);
+			A=init(16,30,99);
 		else if (x=='w'){
 			printf("\npodaj dlugosc: ");
 			scanf("%i",&c);
 			printf("podaj szerokosc: ");
 			scanf("%i",&r);
 		//warunek by realistyczna liczba min
-			printf("podaj ilosc min: ");
-			scanf("%i",&mines);
-			init(r,c,mines);
+			k=1;
+			while(k){
+				k=0;
+				printf("podaj ilosc min: ");
+				scanf("%i",&mines);
+				if(mines>r*c*0.7 || mines <1){
+					printf("nieprawidlowa ilosc min\n");
+					k=1;
+				}
+			}
+			A=init(r,c,mines);
 		}
 		else{
 			printf("nieprawidlowa wartosc wpisz jeszcze raz: ");
 			k=1;
 		}
 	}
+	return A;
+}
+int main()
+{
+	//ziarno
+	srand((unsigned int)time(NULL)^getpid());
+	sap*A=NULL;
+	A=start(A);
+	A=assign(A);
+
+	free_sap(A);
 	return 0;
 }
