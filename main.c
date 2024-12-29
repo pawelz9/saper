24c854f6c9802721b9d0b58a9a19f3fb6691cff8
Delfinek777 <01197814@pw.edu.pl>
2024-12-29 21:51:00 +0100

    zmieniłem nazwy na lepsze i trochę ulepszyłem w komentarzach napisalem

--- main.c
+++ main.c
@@ -2,56 +2,58 @@
 #include <stdlib.h>
 #include<time.h>
 
-typedef struct sap
+typedef struct saper
 {
 	int r;
 	int c;
 	int mines;
 	int **tab;
-}sap ;
+}saper ;
 
-sap *init(int r, int c,int mines ){
-	sap* A=(sap*)malloc(sizeof(sap));
-	A->r=r;
-	A->c=c;
-	A->mines=mines;
-	A->tab=(int**)calloc(r,sizeof(int*));
+saper *init(int r, int c,int mines ){
+	saper* plansza=(saper*)malloc(sizeof(saper));
+	plansza->r=r;
+	plansza->c=c;
+	plansza->mines=mines;
+	plansza->tab=(int**)calloc(r,sizeof(int*));
 	for(int i=0;i<r;i++)
-		A->tab[i]=(int *)calloc(c,sizeof(int));
-	return A;
+		plansza->tab[i]=(int *)calloc(c,sizeof(int));
+	return plansza;
 }
  
-void free_sap(sap* A) {
-    for (int i = 0; i < A->r; i++) {
-        free(A->tab[i]);
+void free_saper(saper* plansza) {
+    for (int i = 0; i < plansza->r; i++) {
+        free(plansza->tab[i]);
     }
-    free(A->tab);
-    free(A);
+    free(plansza->tab);
+    free(plansza);
 }
 
 //w losowa wspolrzedna przypisujemy mine
-sap *assign(sap *A)
+void assign(saper **plansza) //ta funkcja powinna byc voidem, bierze structa i go zapelnia minami i tyle
 {
+	// trzeba to przemyslec czy to musi byc pointer do pointera
+	// w mainie zrobiles pointer do structa mimo ze zwykly struct bylby okej
 	int row,col;
-	for(int i=0;i<A->mines;i++){
-		row=rand()%A->r;
-		col=rand()%A->c;
-		if(A->tab[row][col]==1){
+	for(int i=0;i<(*plansza)->mines;i++){
+		row=rand()%(*plansza)->r;
+		col=rand()%(*plansza)->c;
+		if((*plansza)->tab[row][col]==1){ 
 			i--;
-			continue;
+		}else{
+			(*plansza)->tab[row][col]=1;
 		}
-		A->tab[row][col]=1;
 
 	}
+	
 
-	return A;
 }
 
-sap *start(sap *A){
+saper *start(saper *plansza){
 	char x;
 	int k=1;
 	int r,c,mines;
-	printf ("GRA SAPER\n");
+	printf ("GRA saper\n");
 	printf ("wybierz poziom trudnosci:\n");
 	printf ("l-latwy 9x9 10 min\n");
 	printf ("s-sredni 16x16 40 min\n");
@@ -61,11 +63,11 @@ sap *start(sap *A){
 		k=0;
 		scanf(" %c",&x);
 		if(x=='l')
-			A=init(9,9,10);
+			plansza=init(9,9,10);
 		else if(x=='s')
-			A=init(16,16,40);
+			plansza=init(16,16,40);
 		else if(x=='t')
-			A=init(16,30,99);
+			plansza=init(16,30,99);
 		else if (x=='w'){
 			printf("\npodaj dlugosc: ");
 			scanf("%i",&c);
@@ -82,23 +84,23 @@ sap *start(sap *A){
 					k=1;
 				}
 			}
-			A=init(r,c,mines);
+			plansza=init(r,c,mines);
 		}
 		else{
 			printf("nieprawidlowa wartosc wpisz jeszcze raz: ");
 			k=1;
 		}
 	}
-	return A;
+	return plansza;
 }
 int main()
 {
 	//ziarno
 	srand((unsigned int)time(NULL)^getpid());
-	sap*A=NULL;
-	A=start(A);
-	A=assign(A);
+	saper *plansza;
+	plansza=start(plansza);
+	plansza=assign(plansza);
 
-	free_sap(A);
+	free_saper(plansza);
 	return 0;
 }
